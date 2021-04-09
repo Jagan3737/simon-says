fc9256368b225fc56718823edcb0849a655b7bdd
Jagannathan K <jagan.nathan.jn24@gmail.com>
2021-04-09 11:12:09 +0530

    Initial commit

---
+++ game.js
@@ -0,0 +1,78 @@
+var userClickedPattern = [];
+var gamePattern = [];
+
+var buttonColors = ["red", "blue", "green", "yellow"];
+
+var level = 0;
+var started = false;
+
+$(document).keydown(function() {
+  if (!started) {
+    nextSequence();
+    started = true;
+  }
+});
+
+
+$(".btn").click(function() {
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+
+  checkAnswer(userClickedPattern.length - 1);
+});
+
+
+function nextSequence() {
+  userClickedPattern = [];
+
+  $("#level-title").text("Level " + ++level);
+
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColor = buttonColors[randomNumber];
+  gamePattern.push(randomChosenColor);
+
+  $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+
+  var audio = new Audio("sounds/" + randomChosenColor + ".mp3");
+  audio.play();
+}
+
+function playSound(name) {
+  $("#" + name).fadeIn(100).fadeOut(100).fadeIn(100);
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function animatePress(currentColor) {
+  $("." + currentColor).addClass("pressed");
+  setTimeout(function() {
+    $("." + currentColor).removeClass("pressed");
+  }, 100);
+}
+
+function checkAnswer(currentLevel) {
+  if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
+    console.log("success");
+    if (userClickedPattern.length-1 === gamePattern.length-1) {
+      setTimeout(function() {
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+    playSound("wrong");
+    $("body").addClass("game-over");
+    setTimeout(function() {
+      $("body").removeClass("game-over");
+    }, 200);
+    $("#level-title").text("Game Over, Press Any Key to Restart");
+    startOver();
+  }
+}
+
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  started = false;
+}
